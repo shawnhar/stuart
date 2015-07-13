d6b909ca4c2970463ad6b677894cd50461d60970
Shawn Hargreaves <shawnhar@microsoft.com>
2015-07-13 09:51:54 -0700

    Stub doc class

---
+++ Photo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.Graphics.Canvas;
+
+namespace Stuart
+{
+    class Photo
+    {
+        CanvasDevice device;
+        CanvasBitmap sourceBitmap;
+
+
+        public Photo(CanvasDevice device)
+        {
+            this.device = device;
+        }
+
+
+        public async void Load(string filename)
+        {
+            sourceBitmap = await CanvasBitmap.LoadAsync(device, filename);
+        }
+    }
+}
