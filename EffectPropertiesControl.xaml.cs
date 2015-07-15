862c48ee259226dd92346284f74fcf819bf90cf4
Shawn Hargreaves <shawnhar@microsoft.com>
2015-07-15 00:59:03 -0700

    Basic dynamic UI for effect parameter sliders

--- EffectPropertiesControl.xaml.cs
+++ EffectPropertiesControl.xaml.cs
@@ -41,8 +41,32 @@ namespace Stuart
 
             if (effect != null)
             {
+                var metadata = EffectMetadata.Get(effect.Type);
 
+                for (int i = 0; i < metadata.Count; i++)
+                {
+                    AddEffectParameter(metadata[i], i);
+                }
             }
+        }
+
+
+        void AddEffectParameter(EffectParameter parameter, int row)
+        {
+            var text = new TextBlock { Text = parameter.Name };
+            AddToGrid(text, row, 0);
+
+            var slider = new Slider();
+            AddToGrid(slider, row, 2);
+        }
+
+
+        void AddToGrid(UIElement element, int row, int column)
+        {
+            element.SetValue(Grid.RowProperty, row);
+            element.SetValue(Grid.ColumnProperty, column);
+
+            grid.Children.Add(element);
         }
     }
 }
